38c835b127d6dbd7eb6fb007f33248f00121318c
isayme <isaymeorg@gmail.com>
2015-09-10 23:51:08 +0800

    change regids to regid

--- example/notification.js
+++ example/notification.js
@@ -20,10 +20,16 @@ var notification = new Notification({
 
 var regid = config.regids[0];
 
-notification.send(regid, msg, function() {
+function callback() {
   console.log(JSON.stringify(arguments, null, 2));
-});
+}
 
-// notification.sendToAll(msg, function() {
-//   console.log(JSON.stringify(arguments, null, 2));
-// });
+notification.send(regid, msg, callback);
+
+// notification.sendToAll(msg, callback);
+
+// notification.sendToUserAccount('ua', msg, callback);
+
+// notification.sendToAlias('testAlias', msg, callback);
+
+// notification.sendToTopic('testTopoc', msg, callback);
