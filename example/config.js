6a4dc21a4b67d94b6dc6bc20f062cf78f664706e
isayme <isaymeorg@gmail.com>
2018-03-01 00:27:04 +0800

    update

--- example/config.js
+++ example/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   production: true,
-  restrictedPackageName: 'org.isayme.pushdemo',
-  appSecret: 'fnMrpxXI+BsZEhnmz4OuWg==',
+  restrictedPackageName: 'your package name',
+  appSecret: 'your secret',
   regids: [
-    'd//igwEhgBGCI2TG6lWqlGn8M1ph6bjr4BtnhCZRe0Rb8+ZP0k6fhlIdnC4N2w7hLrlThrzoYjG9In/wojTjsv0OemEa0YpmoWCTD8X7mCg='
+    'your regid'
   ],
   callback: function (err, body) {
     console.log(err)
